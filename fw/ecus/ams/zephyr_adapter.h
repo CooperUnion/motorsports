dca65749cc4bc464175294d33e7c9c030922b526
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-05-21 09:47:46 -0400

    Print message from __ASSERT

--- fw/ecus/ams/zephyr_adapter.h
+++ fw/ecus/ams/zephyr_adapter.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
 uint64_t k_uptime_get(void);
 void k_usleep(uint32_t duration_us);
@@ -12,10 +14,7 @@ enum zeph_errors {
     EIO = -5,
 };
 
-#include <assert.h>
-
-// todo
-#define __ASSERT(a, ...) assert(a)
+#define __ASSERT(a, ...) (assert(a), fprintf(stderr, __VA_ARGS__))
 #define CLAMP(x, low, high) ((x) > (high) ? (high) : ((x) < (low) ? (low) : (x)))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
