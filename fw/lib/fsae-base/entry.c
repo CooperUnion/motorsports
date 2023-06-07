1599d9304ec04ea9f4e44b06c23e226cfd958ed5
Dan Mezhiborsky <daniel.mezhiborsky@cooper.edu>
2023-06-07 13:42:59 -0400

    Working bootloader on PCH

--- fw/lib/fsae-base/entry.c
+++ fw/lib/fsae-base/entry.c
@@ -1,10 +1,14 @@
-#include "ember_bltools.h"
-#include "ember_taskglue.h"
-#include "ember_tasking.h"
 #include "module_list.inc"
+
+#include <stdio.h>
+
+#include <ember_bltools.h>
+#include <ember_taskglue.h>
+#include <ember_tasking.h>
 
 void app_main()
 {
+    printf("### FSAE FIRMWARE BOOT ###");
     /* set boot partition back to bootloader */
     ember_bltools_set_boot_partition_to_factory();
 
