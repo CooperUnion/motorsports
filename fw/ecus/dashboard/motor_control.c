44273441b5cbae686d4500f647b4ff558357de1c
Dan Mezhiborsky <daniel.mezhiborsky@cooper.edu>
2023-06-08 10:07:07 -0400

    Send M192_Command_Message from dash

---
+++ fw/ecus/dashboard/motor_control.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+#include <opencan_tx.h>
+
+void CANTX_populate_M192_Command_Message(struct CAN_Message_M192_Command_Message * const m) {
+    static uint8_t counter = 0;
+    *m = (struct CAN_Message_M192_Command_Message) {
+        .Torque_Command         = 0.0f,
+        .Speed_Command          = 0.0f,
+        .Direction_Command      = CAN_DIRECTION_COMMAND_CW,
+        .Inverter_Enable        = CAN_INVERTER_ENABLE_TURN_THE_INVERTER_OFF,
+        .Inverter_Discharge     = CAN_INVERTER_DISCHARGE_DISCHARGE_DISABLE,
+        .Speed_Mode_Enable      = true,
+        .RollingCounter         = counter++,
+        .Torque_Limit_Command   = 0.0f,
+    };
+}
