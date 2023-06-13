267ce58fa590d2717152f4c9e9bca01b6f2851ee
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-06-13 19:35:31 -0400

    Improve pedal map robustness a bit

--- fw/ecus/dashboard/pedal_map.c
+++ fw/ecus/dashboard/pedal_map.c
@@ -5,19 +5,26 @@
 // simple torque to percentage pedal map.
 // linear interpolation
 
+#define TORQUE_RATIONALITY_LIMIT    (100.0f)
+
 struct pedal_map_point {
     float percent;
     float torque;
 };
 
 static struct pedal_map_point pedal_map[] = {
-    {0.0, 0.0},     // 0%, 0 Nm
-    {0.25, 10.0},   // 25%, 20 Nm
-    {1.01, 100.0},   // 100% 100 Nm
+    {0.0f, 0.0f},       // 0%, 0 Nm
+    {0.25f, 10.0f},     // 25%, 20 Nm
+    {1.01f, 100.0f},    // 100% 100 Nm
 };
 
 float pedal_to_torque(float pedal_percent) {
     float torque = 0.0;
+
+    // input rationality
+    if (pedal_percent < 0.0f || pedal_percent > 1.0f) {
+        return 0.0f;
+    }
 
     for (size_t i = 0; i < ARRAY_SIZE(pedal_map) - 1; i++) {
         const struct pedal_map_point region_min = pedal_map[i];
@@ -42,9 +49,9 @@ float pedal_to_torque(float pedal_percent) {
         }
     }
 
-    // rationality check
-    if (torque < 0.0f || torque > 100.0f) {
-        torque = 0.0f;
+    // final rationality check
+    if (torque < 0.0f || torque > TORQUE_RATIONALITY_LIMIT) {
+        return 0.0f;
     }
 
     return torque;
